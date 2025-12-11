c4091a5aaef18f44413ac539d09bc2b815de577f
Njesa Totty <43484269+tottyn@users.noreply.github.com>
2025-12-11 16:17:24 +0000

    Latest build deployed.

---
+++ course/lunr-pretext-search-index.js
@@ -0,0 +1,167 @@
+var ptx_lunr_search_style = "textbook";
+var ptx_lunr_docs = [
+{
+  "id": "syllabus",
+  "level": "1",
+  "url": "syllabus.html",
+  "type": "Section",
+  "number": "",
+  "title": "Syllabus",
+  "body": " Syllabus   Welcome to what promises to be an exciting and fun semester of ...     Course Information  This is the syllabus for course name (MATH xxx, section xxx) for semester 20xx. It is a x credit course.   Instructor  Prof. Lastname, Office Location, prof.lastname@example.edu .    Student Hours  TBD   Important : I want to see you in student hours, and will happily make appointments with you outside of the regular hours. I'm also available by email and will respond within 24 hours, usually much sooner. There is little I enjoy more than discussing mathematics, so you are really doing me a favor by coming to see me.    Class meets  course times and location.    Course Description  course description from catalog    Prerequisite  list of prerequisites    Textbook and course materials   textbook name by textbook author.      Course Overview       Assessments and Grades     Learning targets  By the end of this course, you should be able to:              Assessment of Learning Targets      Final Exam      Assessment of Mathematical Engagement  Your level of effort and engagement will be assessed through your participation in class and completion of a variety of homework assignments.   Participation      Daily Prep Assignments      Practice Problems      Written Homework         Final Grades        Course Policies   Attendance      Late work      Classroom Decorum      Academic Integrity      Generative AI      Title IX and Equal Opportunity       Important Dates     Friday, Aug 25  Last day to add a class    Monday, Sep 4  Labor Day (no classes)    Monday, Sep 8  Last day to drop a class    Nov 26 - Nov 28  Thanksgiving break (no classes Wednesday, Thursday or Friday)    Friday, Dec 5  Last day to withdraw from class and receive a W    Thursday, Dec 11  Final Exam from 10:45am to 1:15pm      "
+},
+{
+  "id": "sec-course-info-2",
+  "level": "2",
+  "url": "syllabus.html#sec-course-info-2",
+  "type": "Paragraph (with a defined term)",
+  "number": "",
+  "title": "",
+  "body": "course name (MATH xxx, section xxx) "
+},
+{
+  "id": "notes-week-01",
+  "level": "1",
+  "url": "notes-week-01.html",
+  "type": "Section",
+  "number": "",
+  "title": "Week 1",
+  "body": " Week 1   This is an outline of the topics we covered in the first week of class.     Monday 8\/22      Wednesday 8\/24      Friday 8\/26     "
+},
+{
+  "id": "notes-week-02",
+  "level": "1",
+  "url": "notes-week-02.html",
+  "type": "Section",
+  "number": "",
+  "title": "Week 2",
+  "body": " Week 2   Monday      Wednesday      Friday     "
+},
+{
+  "id": "activity-00-sample-activity",
+  "level": "1",
+  "url": "activity-00-sample-activity.html",
+  "type": "Worksheet",
+  "number": "",
+  "title": "Sample Activity: Relations and Digraphs",
+  "body": " Sample Activity: Relations and Digraphs     Each matrix below represents a relation. The rows and columns are numbered through or . Give the arrow diagram for each matrix, then express each relation as a set of ordered pairs.                   From the given matrix, we see that , and . Therefore, the set of ordered pairs for this relation is . The arrow diagram is shown below at left.   From the given matrix, we see that and . Hence, the set of ordered pairs for this relation is . The arrow diagram is shown below at center.  Reading the matrix, we see , and . The set of ordered pairs will therefore be . The arrow diagram is shown below at right.                                                       Give the matrix representation for the relation depicted in each arrow diagram. Then express the relation as a set of ordered pairs.                                                                    From the diagram, we deduce that the set of ordered pairs representing this relation will be . The matrix representation will thus be .    The relation as a set of ordered pairs can be expressed as . The matrix representation is .    As a set of ordered pairs, the relation can be described as . The matrix representation will be .         Draw the arrow diagram and the matrix representation for each relation.     The domain for relation is and .                    The arrow diagram is shown at left. The matrix representation is .       The domain for relation is , and .                  The arrow diagram is shown at left. The matrix representation is .        In this problem, state the definitions of the following properties of binary relations: Reflexive, Anti-reflexive or Irreflexive, Symmetric, Antisymmetric, and Transitive.    For all of these, let be a binary relation on a set .   Reflexive:   is reflexive if and only if for every , .    Anti-Reflexive or Irreflexive:   is anti-reflexive or irreflexive if and only if for every , (i.e., is not related to itself).    Symmetric:   is symmetric if and only if for every all , if and only if . (Note that this also means that is symmetric if and only if for all , if and only if .)    Antisymmetric:   is antisymmetric if and only if for all , if , then and cannot both be true. (So, for every pair of distinct elements in , one of the following holds: but , but , or and .)    Transitive   is transitive if and only if for all , if and , then . (Note that these don't necessarily have to be distinct.)           For each of the following problems, consider the given relation defined on a set . Determine whether satisfies any of the properties defined in Problem 4 above. If a property does not hold, say why.      on the set .    The relation is reflexive, symmetric, and transitive. Clearly it is not irreflexive. Moreover, is not antisymmetric because there is a distinct pair of elements where and are both true.      Consider the relation defined on as if .    Since , . Then this relation is clearly going to be reflexive, symmetric, and transitive.      Consider the relation defined on where we declare if and only if and have the same parity.    This relation is reflexive because clearly will have the same parity as itself, so . Also, is symmetric because if , so and have the same parity, then and will also have the same parity, so . The relation is transitive because if and , then and have the same parity and and have the same parity, so and thus have the same parity. Hence, . (Notice that is actually just congruence modulo 2.)       A digraph      N=6  f(t)=(cos(2*pi*t\/N), sin(2*pi*t\/N))    a  b  c  d  e  f  g          Answer the following questions about the digraph in .     Which vertex has the largest in-degree? What is the in-degree for that vertex?    The vertex with the largest in-degree is , which has degree .      Which vertex has the largest out-degree? What is the out-degree for that vertex?    The vertex with the largest out-degree is , which has degree .      List all self-loops in the digraph.    The self-loops in this digraph are .        Answer the following questions about the digraph in .     Is the sequence a walk in the graph? If so, is it an open walk?    Yes, this sequence is a walk, and it is an open walk because the first and last vertices ( and , respectively) are different.      Is the sequence a walk in the graph? If so, is it an open or closed walk? Is it a trail, path, circuit, or cycle?    This sequence is not a walk because is not an edge in the given graph. Since this is not a walk, it automatically cannot be an open or closed walk, or a trail, path, circuit, or cycle.      Is the sequence a walk in the graph? If so, is it an open or closed walked? Is it a trail, path, circuit, or cycle?      "
+},
+{
+  "id": "activity-00-sample-activity-2-1",
+  "level": "2",
+  "url": "activity-00-sample-activity.html#activity-00-sample-activity-2-1",
+  "type": "Worksheet Exercise",
+  "number": "1",
+  "title": "",
+  "body": "  Each matrix below represents a relation. The rows and columns are numbered through or . Give the arrow diagram for each matrix, then express each relation as a set of ordered pairs.                   From the given matrix, we see that , and . Therefore, the set of ordered pairs for this relation is . The arrow diagram is shown below at left.   From the given matrix, we see that and . Hence, the set of ordered pairs for this relation is . The arrow diagram is shown below at center.  Reading the matrix, we see , and . The set of ordered pairs will therefore be . The arrow diagram is shown below at right.                                                    "
+},
+{
+  "id": "activity-00-sample-activity-2-2",
+  "level": "2",
+  "url": "activity-00-sample-activity.html#activity-00-sample-activity-2-2",
+  "type": "Worksheet Exercise",
+  "number": "2",
+  "title": "",
+  "body": "  Give the matrix representation for the relation depicted in each arrow diagram. Then express the relation as a set of ordered pairs.                                                                    From the diagram, we deduce that the set of ordered pairs representing this relation will be . The matrix representation will thus be .    The relation as a set of ordered pairs can be expressed as . The matrix representation is .    As a set of ordered pairs, the relation can be described as . The matrix representation will be .      "
+},
+{
+  "id": "activity-00-sample-activity-2-3",
+  "level": "2",
+  "url": "activity-00-sample-activity.html#activity-00-sample-activity-2-3",
+  "type": "Worksheet Exercise",
+  "number": "3",
+  "title": "",
+  "body": "  Draw the arrow diagram and the matrix representation for each relation.     The domain for relation is and .                    The arrow diagram is shown at left. The matrix representation is .       The domain for relation is , and .                  The arrow diagram is shown at left. The matrix representation is .     "
+},
+{
+  "id": "activity-00-sample-activity-2-4",
+  "level": "2",
+  "url": "activity-00-sample-activity.html#activity-00-sample-activity-2-4",
+  "type": "Worksheet Exercise",
+  "number": "4",
+  "title": "",
+  "body": "  In this problem, state the definitions of the following properties of binary relations: Reflexive, Anti-reflexive or Irreflexive, Symmetric, Antisymmetric, and Transitive.    For all of these, let be a binary relation on a set .   Reflexive:   is reflexive if and only if for every , .    Anti-Reflexive or Irreflexive:   is anti-reflexive or irreflexive if and only if for every , (i.e., is not related to itself).    Symmetric:   is symmetric if and only if for every all , if and only if . (Note that this also means that is symmetric if and only if for all , if and only if .)    Antisymmetric:   is antisymmetric if and only if for all , if , then and cannot both be true. (So, for every pair of distinct elements in , one of the following holds: but , but , or and .)    Transitive   is transitive if and only if for all , if and , then . (Note that these don't necessarily have to be distinct.)      "
+},
+{
+  "id": "activity-00-sample-activity-3-1",
+  "level": "2",
+  "url": "activity-00-sample-activity.html#activity-00-sample-activity-3-1",
+  "type": "Worksheet Exercise",
+  "number": "5",
+  "title": "",
+  "body": "  For each of the following problems, consider the given relation defined on a set . Determine whether satisfies any of the properties defined in Problem 4 above. If a property does not hold, say why.      on the set .    The relation is reflexive, symmetric, and transitive. Clearly it is not irreflexive. Moreover, is not antisymmetric because there is a distinct pair of elements where and are both true.      Consider the relation defined on as if .    Since , . Then this relation is clearly going to be reflexive, symmetric, and transitive.      Consider the relation defined on where we declare if and only if and have the same parity.    This relation is reflexive because clearly will have the same parity as itself, so . Also, is symmetric because if , so and have the same parity, then and will also have the same parity, so . The relation is transitive because if and , then and have the same parity and and have the same parity, so and thus have the same parity. Hence, . (Notice that is actually just congruence modulo 2.)    "
+},
+{
+  "id": "fig-wkst-12-digraph",
+  "level": "2",
+  "url": "activity-00-sample-activity.html#fig-wkst-12-digraph",
+  "type": "Figure",
+  "number": "2",
+  "title": "",
+  "body": " A digraph      N=6  f(t)=(cos(2*pi*t\/N), sin(2*pi*t\/N))    a  b  c  d  e  f  g       "
+},
+{
+  "id": "activity-00-sample-activity-3-2-2",
+  "level": "2",
+  "url": "activity-00-sample-activity.html#activity-00-sample-activity-3-2-2",
+  "type": "Worksheet Exercise",
+  "number": "6",
+  "title": "",
+  "body": "  Answer the following questions about the digraph in .     Which vertex has the largest in-degree? What is the in-degree for that vertex?    The vertex with the largest in-degree is , which has degree .      Which vertex has the largest out-degree? What is the out-degree for that vertex?    The vertex with the largest out-degree is , which has degree .      List all self-loops in the digraph.    The self-loops in this digraph are .    "
+},
+{
+  "id": "activity-00-sample-activity-3-3",
+  "level": "2",
+  "url": "activity-00-sample-activity.html#activity-00-sample-activity-3-3",
+  "type": "Worksheet Exercise",
+  "number": "7",
+  "title": "",
+  "body": "  Answer the following questions about the digraph in .     Is the sequence a walk in the graph? If so, is it an open walk?    Yes, this sequence is a walk, and it is an open walk because the first and last vertices ( and , respectively) are different.      Is the sequence a walk in the graph? If so, is it an open or closed walk? Is it a trail, path, circuit, or cycle?    This sequence is not a walk because is not an edge in the given graph. Since this is not a walk, it automatically cannot be an open or closed walk, or a trail, path, circuit, or cycle.      Is the sequence a walk in the graph? If so, is it an open or closed walked? Is it a trail, path, circuit, or cycle?    "
+},
+{
+  "id": "sample-handout",
+  "level": "1",
+  "url": "sample-handout.html",
+  "type": "Handout",
+  "number": "",
+  "title": "Sample Handout: Derivative Rules",
+  "body": " Sample Handout: Derivative Rules    Rules for specific types of functions    Constant functions       Power functions       Exponential functions          Logarithmic functions       Trigonometric functions   .   .   .   .   .      Rules for combinations of functions    Constant multiples       Sum and difference       Products (the product rule)       Quotients (the quotient rule)       Compositions (the chain rule)         "
+},
+{
+  "id": "hw1",
+  "level": "1",
+  "url": "hw1.html",
+  "type": "Worksheet",
+  "number": "",
+  "title": "Sample Homework: Written Homework 1",
+  "body": " Sample Homework: Written Homework 1    Instructions: Complete all the parts below on a separate page (not between the prompts). Submit your work by uploading a single PDF to Canvas. This can either be a scan of handwritten solutions or a PDF you created by first typing your solutions.  Note that the last prompt asks you to write a project proposal . This should be written out in paragraph form. Pretend you are really trying to earn the business of the farmer, so make it professional and accurate.      A local farmer has reached out to your family's livestock containment company for an estimate to create a habitat for their new emus. The requirements given to you by the farmer are:   Create a rectangular habitat that is 4000 square feet total, divided into two equal sized areas.    Exterior fence must be 6' tall sturdy 12 gauge galvanized steel.    Interior fence to divide the two areas can be 4' tall and as needs to only be 16 gauge.    Since emu's like to run, it is preferable to have one dimension of the individual areas be at least 100' long.   The farmer has requested a project plan, including estimates, for the habitat. Your dad has asked you to use calculus to help write the proposal.  On your end, you know that including parts and labor, the exterior fence will cost $15 per foot, while the interior fence costs only $10 per foot.     Start by drawing a schematic for the project.      Suppose one dimension is exactly 100'. Find the other dimension and compute the exact cost for this version of the habitat.      Is there a way to reduce the cost for the farmer? Create a function that gives the total cost of a project when the long side of the habitat is feet long. Verify that is the same cost as you found in the previous part.    You will likely want to first write in terms of two dimensions. Then use the fact that the total area of the habitat must be 4000 square feet to eliminate one variable.      Sketch a graph of the function on an appropriate domain.      Use calculus to find the absolute minimum of the function , both with and without the restriction that one side of the habitat must be 100' long.      Use what you have found in the above parts to write a self contained project proposal, that includes at least two options for the farmer. Clearly explain what the options are, how much they would cost, and what the farmer would get with each. Remember, you are trying to sell fence here. Don't dissapoint your dad!      "
+},
+{
+  "id": "hw1-2-2",
+  "level": "2",
+  "url": "hw1.html#hw1-2-2",
+  "type": "Paragraph (with a defined term)",
+  "number": "",
+  "title": "",
+  "body": "project proposal "
+},
+{
+  "id": "hw1-3-1",
+  "level": "2",
+  "url": "hw1.html#hw1-3-1",
+  "type": "Project",
+  "number": "1",
+  "title": "",
+  "body": "  A local farmer has reached out to your family's livestock containment company for an estimate to create a habitat for their new emus. The requirements given to you by the farmer are:   Create a rectangular habitat that is 4000 square feet total, divided into two equal sized areas.    Exterior fence must be 6' tall sturdy 12 gauge galvanized steel.    Interior fence to divide the two areas can be 4' tall and as needs to only be 16 gauge.    Since emu's like to run, it is preferable to have one dimension of the individual areas be at least 100' long.   The farmer has requested a project plan, including estimates, for the habitat. Your dad has asked you to use calculus to help write the proposal.  On your end, you know that including parts and labor, the exterior fence will cost $15 per foot, while the interior fence costs only $10 per foot.     Start by drawing a schematic for the project.      Suppose one dimension is exactly 100'. Find the other dimension and compute the exact cost for this version of the habitat.      Is there a way to reduce the cost for the farmer? Create a function that gives the total cost of a project when the long side of the habitat is feet long. Verify that is the same cost as you found in the previous part.    You will likely want to first write in terms of two dimensions. Then use the fact that the total area of the habitat must be 4000 square feet to eliminate one variable.      Sketch a graph of the function on an appropriate domain.      Use calculus to find the absolute minimum of the function , both with and without the restriction that one side of the habitat must be 100' long.      Use what you have found in the above parts to write a self contained project proposal, that includes at least two options for the farmer. Clearly explain what the options are, how much they would cost, and what the farmer would get with each. Remember, you are trying to sell fence here. Don't dissapoint your dad!    "
+}
+]
+
+var ptx_lunr_idx = lunr(function () {
+  this.ref('id')
+  this.field('title')
+  this.field('body')
+  this.metadataWhitelist = ['position']
+
+  ptx_lunr_docs.forEach(function (doc) {
+    this.add(doc)
+  }, this)
+})
